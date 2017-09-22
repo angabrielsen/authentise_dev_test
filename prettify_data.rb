90343e55d71b3f341601997b22c6d1c88871e4d8
Ashton Gabrielsen <ashton.gabrielsen@i28387-IS4.local>
2017-09-21 23:40:45 -0600

    tests data is output as an array of individual objects

--- prettify_data.rb
+++ prettify_data.rb
@@ -1,14 +1,20 @@
-def parse_data
-  data = File.read("./machine_log.txt")
-    .strip
-    .gsub(/\r\n?/, "\n")
-    .split(";")
+class Prettify
+	def parse_data
+	  data = File.read("./machine_log.txt")
+	    .strip
+	    .gsub(/\r\n?/, "\n")
+	    .split(";")
 
-  parsedData = data.map {
-    |item| item.strip.split
-  }
+	  parsedData = data.map {
+	    |item| item.strip.split(";")
+	  }
 
-  puts parsedData.inspect
+	  parsedData
+	end
 end
 
-parse_data
+=begin
+execute from command line
+
+ruby -r "./prettify_data.rb" -e "Prettify.new.parse_data"
+=end
--- spec/prettify_data_spec.rb
+++ spec/prettify_data_spec.rb
@@ -1,5 +1,19 @@
-require './devTest.rb'
+require './prettify_data.rb'
 
-RSpec.describe DevTest, "#parse_data" do
-	
+RSpec.describe Prettify, "#parse_data" do
+  context "when reading local file" do
+    it "prints an array" do
+      prettify = Prettify.new
+      data = prettify.parse_data
+
+      expect(data.class).to eq Array
+    end
+
+    it "contains 6 objects" do
+      prettify = Prettify.new
+      data = prettify.parse_data
+
+      expect(data.length).to eq 6
+    end
+  end
 end
