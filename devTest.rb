e304782e7cfa8a08864ee8c16557e81f5436a466
Ashton Gabrielsen <ashton.gabrielsen@i28387-IS4.local>
2017-09-21 21:45:21 -0600

    install rspec

--- devTest.rb
+++ devTest.rb
@@ -1,6 +1,8 @@
-
 def parse_data
-  data = File.read("./Sample-stratasys-status-log.txt").strip.gsub(/\r\n?/, "\n").split(";")
+  data = File.read("./Sample-stratasys-status-log.txt")
+    .strip
+    .gsub(/\r\n?/, "\n")
+    .split(";")
 
   parsedData = data.map {
     |item| item.strip.split
